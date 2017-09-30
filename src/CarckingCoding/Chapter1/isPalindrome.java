892a393e5160e5b54bb0427039885a8a39f2ff97
Sungin Jung <jungs@uchicago.edu>
2017-09-29 22:04:49 -0500

    9/29 Java Algo

---
+++ src/CarckingCoding/Chapter1/isPalindrome.java
@@ -0,0 +1,37 @@
+package CarckingCoding.Chapter1;
+
+/**
+ * Created by sunginjung on 9/29/17.
+ */
+public class isPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("!!!!Are we not pure? “No, sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man—a prisoner up to new era."));
+    }
+
+    static boolean isPalindrome(String s){
+        if (s.isEmpty()) {
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if(!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+
+        return true;
+    }
+}
+
