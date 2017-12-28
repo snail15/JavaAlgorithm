c1f1fd439530e062ac3f8c361ee7330e37c9aa81
Sungin Jung <jungs@uchicago.edu>
2017-12-28 04:45:34 -0600

    12/27 Java LinkedList done

---
+++ src/MadeForJava/Queue/QueueByStack.java
@@ -0,0 +1,43 @@
+package MadeForJava.Queue;
+
+import MadeForJava.LinkedList.SingleNode;
+
+import java.util.Stack;
+
+/**
+ * Created by sunginjung on 12/28/17.
+ */
+public class QueueByStack {
+
+    private Stack<SingleNode> inputStack;
+    private Stack<SingleNode> outputStack;
+
+    public QueueByStack() {
+        this.inputStack = new Stack<>();
+        this.outputStack = new Stack<>();
+    }
+
+    public boolean isEmpty() {
+
+        return (this.inputStack.size() == 0 && this.outputStack.size() == 0);
+    }
+
+    private void shiftStacks() {
+
+        if(this.outputStack.isEmpty()) {
+            while(!this.inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+    }
+
+    public int front() {
+        shiftStacks();
+        return this.outputStack.peek().getData();
+    }
+
+    public SingleNode dequeue() {
+        shiftStacks();
+        return this.outputStack.pop();
+    }
+}
