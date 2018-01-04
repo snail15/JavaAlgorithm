d865eabddbd9872aaba72ec9efe22425d6405f1a
Sungin Jung <jungs@uchicago.edu>
2018-01-04 14:01:25 -0600

    1/4 Java Algo

---
+++ src/LeetCode/MaxDepthBinaryTree.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+/**
+ * Created by sunginjung on 1/4/18.
+ */
+public class MaxDepthBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        return root==null? 0 : Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    }
+}
